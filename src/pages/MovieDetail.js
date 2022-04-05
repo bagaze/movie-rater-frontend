04aa5acb9201e78e9c4013e208f07e8f916c6348
Baptiste GAZE <bpt.gaze@gmail.com>
2022-04-05 11:44:29 +0200

    Correct release date in MovieDetail

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -15,7 +15,7 @@ function MovieDetail() {
 
     useEffect( () => {
         const customQueryParams = {
-            append_to_response: "credits"
+            append_to_response: "credits,release_dates"
         };
 
         getTMDBData(
@@ -28,13 +28,28 @@ function MovieDetail() {
     }, [tmdbid]);
 
     let directors = []
+    let releaseDateFR = "";
 
     if (movie) {
+        // Extract list of directors
         directors = movie.credits.crew.reduce( (acc, crew_member) => (
             crew_member.job === "Director" ? [...acc, crew_member.name] : acc
         ), []);
         console.log("directors");
         console.log(directors);
+
+        // Extract french release date in theater
+        console.log(movie.release_dates.results)
+        movie.release_dates.results.forEach(releaseDateResult => {
+            if (releaseDateResult.iso_3166_1 === "FR") {
+                releaseDateResult.release_dates.forEach( (releaseDate) => {
+                    console.log(releaseDate)
+                    if (releaseDate.type === 3) {
+                        releaseDateFR = new Date(releaseDate.release_date).toDateString();
+                    }
+                } );
+            }
+        });
     }
 
     if (error) {
@@ -54,8 +69,8 @@ function MovieDetail() {
                         alt={`poster ${movie.title}`}
                     /><br />
                     <span>Title: {movie.title}</span><br />
-                    <span>Director(s): {directors.join(', ')}</span><br />
-                    <span>Release date: {movie.release_date}</span>
+                    <span>Director(s): {directors.join(', ') || "TBD"}</span><br />
+                    <span>Release date: {releaseDateFR || "TDB"}</span>
                 </div>
             ) }
         </div>
